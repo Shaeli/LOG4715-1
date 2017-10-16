5f27cd9bf3340ad4ea893e9a59e8bf235858ecda
Shaeli <eleonore.carpentier@insa-cvl.fr>
2017-10-16 11:15:22 -0400

    teleportation+pickup

---
+++ TP2/Assets/Scripts/DiamondsRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondsRotation : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 45, 5) * Time.deltaTime * 2);
+    }
+}
