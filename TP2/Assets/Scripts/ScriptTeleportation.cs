47cbabec3573cd673c0a814e6ea63c54e57ec508
Sgravis <samuel.gravis@insa-cvl.fr>
2017-11-04 09:27:57 -0400

    Tp fantome perso

--- TP2/Assets/Scripts/ScriptTeleportation.cs
+++ TP2/Assets/Scripts/ScriptTeleportation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScriptTeleportation : MonoBehaviour
@@ -10,6 +11,9 @@ public class ScriptTeleportation : MonoBehaviour
     //cooldown en cours
     [SerializeField] float RefillSpeed = 1f;
     [SerializeField] Image FloatingImagefill;
+
+    [SerializeField] GameObject showTPRight;
+    [SerializeField] GameObject showTPLeft;
     bool coolDown;
     //Distance téléportation
     [SerializeField]
@@ -20,6 +24,8 @@ public class ScriptTeleportation : MonoBehaviour
     // Awake se produit avait le Start. Il peut être bien de régler les références dans cette section.
     void Awake()
     {
+        showTPRight.SetActive(false);
+        showTPLeft.SetActive(false);
         coolDown = false;
         FloatingImagefill.fillAmount = 1;
     }
@@ -34,28 +40,47 @@ public class ScriptTeleportation : MonoBehaviour
 
     void CheckTeleportation()
     {
-        if (Input.GetButtonDown("Teleportation") && !coolDown)
+      if (!coolDown)
+      {
+        if (Input.GetButtonDown("Teleportation"))
         {
-            if (gameObject.GetComponent<PlayerControler>()._Flipped)
-            {
-                transform.position += new Vector3(0, 0, -TeleportationDistance);
-                FloatingImagefill.GetComponent<Image>().fillAmount = 0;
-                coolDown = true;
-            }
-            else
-            {
-                transform.position += new Vector3(0, 0, TeleportationDistance);
-                FloatingImagefill.GetComponent<Image>().fillAmount = 0;
-                coolDown = true;
-            }
+          if (gameObject.GetComponent<PlayerControler>()._Flipped)
+          {
+            showTPLeft.transform.position += new Vector3(-TeleportationDistance * 50, 0, 0);
+            showTPLeft.SetActive(true);
+          }
+          else
+          {
+            showTPRight.transform.position += new Vector3(TeleportationDistance * 50, 0, 0);
+            showTPRight.SetActive(true);
+          }
         }
+        if (Input.GetButtonUp("Teleportation"))
+        {
+          if (gameObject.GetComponent<PlayerControler>()._Flipped)
+          {
+            transform.position += new Vector3(0, 0, -TeleportationDistance);
+            FloatingImagefill.GetComponent<Image>().fillAmount = 0;
+            coolDown = true;
+            showTPLeft.SetActive(false);
+            showTPRight.SetActive(false);
+          }
+          else
+          {
+            transform.position += new Vector3(0, 0, TeleportationDistance);
+            FloatingImagefill.GetComponent<Image>().fillAmount = 0;
+            coolDown = true;
+            showTPRight.SetActive(false);
+            showTPLeft.SetActive(false);
+          }
+        }
+      }
     }
 
     void TeleportationCoolDown()
     {
         if (coolDown)
         {
-            print("oui");
             Refill += Time.deltaTime * RefillSpeed;
             FloatingImagefill.GetComponent<Image>().fillAmount = Refill / timecd;
             if (FloatingImagefill.fillAmount == 1)
