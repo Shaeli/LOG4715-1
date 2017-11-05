f1e54a991d51f8cfff73049b8a67cdf1711d3b70
David Tran <dt_dbqt@hotmail.com>
2017-11-04 21:07:53 -0400

    done activables

---
+++ TP2/Assets/Scripts/Activables/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    [SerializeField]
+    bool IsReusable = true;
+
+    [SerializeField]
+    bool StartingState = false;
+
+    [SerializeField]
+    Animator SwitchAnimator;
+
+    public Activable ActivableObject;
+
+    private bool state;
+    private bool isUsed;
+
+    void Start()
+    {
+        state = StartingState;
+        isUsed = false;
+    }
+
+    public void TriggerSwitch()
+    {
+        // Already used an unreusable.
+        if (!IsReusable && isUsed) { return; }
+
+        isUsed = true;
+        state = !state;
+        SwitchAnimator.SetBool("State", state);
+        ActivableObject?.Activate();
+    }
+}
