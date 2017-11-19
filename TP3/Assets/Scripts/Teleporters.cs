bf65e74382e210e9aa0f93670fd3e1d543b756ab
Sgravis <samuel.gravis@insa-cvl.fr>
2017-11-19 11:05:51 -0500

    Teleporter script

---
+++ TP3/Assets/Scripts/Teleporters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporters : MonoBehaviour {
+
+
+	[SerializeField] Vector3 initialPosition;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.transform.position = initialPosition;
+		}
+	}
+}
