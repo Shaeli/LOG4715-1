8b3836aa57d952289def7604ddd208c692f4fcfa
David Tran <dt_dbqt@hotmail.com>
2017-11-19 21:21:53 -0500

    menu almost done

---
+++ TP3/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    [SerializeField]
+    string SingleLevel;
+    [SerializeField]
+    string MultiLevel;
+
+	public void LoadSingleLevel()
+    {
+        if (SingleLevel != null)
+            SceneManager.LoadScene(SingleLevel);
+    }
+
+    public void LoadMultiLevel()
+    {
+        if (MultiLevel != null)
+            SceneManager.LoadScene(MultiLevel);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
