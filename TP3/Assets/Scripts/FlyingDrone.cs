ff2d4c3b2a19cc7a1ad396dc4ce633bec687f15a
Yawen <sytta@live.ca>
2017-11-27 02:26:52 -0500

    Enemies ready

---
+++ TP3/Assets/Scripts/FlyingDrone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingDrone : MonoBehaviour {
+
+    private Rigidbody rb;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private Transform top;
+    [SerializeField] private Transform bottom;
+    private bool goDown = true;
+    private bool goUp = false;
+
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        float step = Time.deltaTime * speed;
+        if (transform.position.y > bottom.position.y && goDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, bottom.position + new Vector3(0, -0.1f, 0), step);
+            if (transform.position.y < bottom.position.y)
+            {
+                goUp = true;
+                goDown = false;
+            }
+        } else if (transform.position.y < top.position.y && goUp)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, top.position + new Vector3(0, 0.1f, 0), step);
+            if (transform.position.y > top.position.y)
+            {
+                goUp = false;
+                goDown = true;
+            }
+        }
+        
+    }
+
+    
+}
---
+++ TP3/Assets/Scripts/Laser.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    [SerializeField] private Transform turret;
+    private LineRenderer lr;
+    private ParticleSystem ps;
+    [SerializeField] private float shootInterval = 3f;
+    [SerializeField] private float shootingTime = 2f;
+    [SerializeField] private float psTime = 0.5f;
+    private bool isShooting;
+    private float currentTime;
+    private bool isPlayingPs;
+
+	// Use this for initialization
+	void Start () {
+        isShooting = false;
+        isPlayingPs = false;
+        currentTime = 0f;
+        lr = GetComponent<LineRenderer>();
+        ps = GetComponent<ParticleSystem>();
+        lr.startWidth = 0.2f;
+        lr.endWidth = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        checkShootingTime();
+
+        if (isShooting)
+        {
+            Shoot();
+        }
+        else if (isPlayingPs)
+        {
+            PlayParticleSystem();
+        }
+        else
+        {
+            Patrol();
+        }
+	}
+
+    void checkShootingTime()
+    {
+        currentTime += Time.deltaTime;
+        if (!isShooting && !isPlayingPs)
+        {
+            if (currentTime >= shootInterval)
+            {
+                currentTime = 0;
+                isPlayingPs = true;
+            }
+        }
+        else if (isPlayingPs)
+        {
+            if (currentTime >= psTime)
+            {
+                isPlayingPs = false;
+                isShooting = true;
+            }
+        }
+        else if (isShooting)
+        {
+            if (currentTime >= shootingTime)
+            {
+                currentTime = 0;
+                isShooting = false;
+            }
+        }
+
+    }
+
+    void Patrol()
+    {
+        lr.enabled = false;
+    }
+
+    void PlayParticleSystem()
+    {
+        // Play particle system before shooting
+        if (!ps.isPlaying)
+        {
+            ps.Play();
+        }
+    }
+
+    void Shoot()
+    {
+        // Head, position
+        lr.SetPosition(0, turret.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+
+                // Check if it is player
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                {
+                    DamagingObject damageObject = new DamagingObject();
+                    damageObject.Damage = 1;
+                    hit.transform.SendMessage("GetDamaged", damageObject);
+                }
+            }
+        }
+        else
+        {
+            // Tail position
+            lr.SetPosition(1, transform.forward * 5000);
+        }
+
+        lr.enabled = true;
+    }
+}
