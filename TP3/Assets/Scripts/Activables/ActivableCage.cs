82952ed9ab56c2c9faf9d99915b47ef4c72f3acf
Shaeli <eleonore.carpentier@insa-cvl.fr>
2017-11-27 10:41:22 -0500

    ajout de textures

---
+++ TP3/Assets/Scripts/Activables/ActivableCage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivableCage : Activable {
+
+
+    [SerializeField]
+    GameObject cage;
+
+    [SerializeField]
+    bool StartingState;
+
+    private bool state;
+
+    // Use this for initialization
+    void Start()
+    {
+        state = StartingState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state)
+        {
+            cage.SetActive(false);
+        }
+        else
+        {
+            cage.SetActive(true);
+        }
+    }
+
+    public override void Activate()
+    {
+        state = !state;
+    }
+}
