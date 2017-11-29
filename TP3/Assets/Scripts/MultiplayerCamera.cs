4d2d5027550c06eebab685b1786712d9ba36b9f5
Yawen <sytta@live.ca>
2017-11-28 20:19:47 -0500

    Added multiplayer camera and changed Energy canvas

--- TP3/Assets/Scripts/MultiplayerCamera.cs
+++ TP3/Assets/Scripts/MultiplayerCamera.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MultiplayerCamera : MonoBehaviour {
 
-    
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private float zoomFactor;
+
+    private float initialZoom;
+    private float initialDistance;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Vector3 middle = (player1.position + player2.position) * 0.5f;
+
+        initialZoom = (middle - transform.position).magnitude;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        FollowPlayers();
 	}
+
+    void FollowPlayers()
+    {
+        // Middle point 
+        Vector3 middle = (player1.position + player2.position) * 0.5f;
+
+        // To calculate the zoom of the camera
+        float zoomDistance = (player1.position - player2.position).magnitude * zoomFactor;
+
+        // Move camera a certain distance
+        Vector3 cameraDestination;
+
+        if (zoomDistance > initialZoom)
+            cameraDestination = middle - transform.forward * zoomDistance;
+        else
+            cameraDestination = middle - transform.forward * initialZoom;
+
+        transform.position = Vector3.Slerp(transform.position, cameraDestination, Time.deltaTime);
+
+    }
 }
