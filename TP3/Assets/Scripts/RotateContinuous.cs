bab8f56dbc977ea83a3e0f3d14d9dc216ddfbb91
David Tran <dt_dbqt@hotmail.com>
2017-11-30 19:06:30 -0500

    scene solo polished

---
+++ TP3/Assets/Scripts/RotateContinuous.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateContinuous : MonoBehaviour {
+
+    [SerializeField] Vector3 Rate;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Rate * Time.deltaTime);
+	}
+}
